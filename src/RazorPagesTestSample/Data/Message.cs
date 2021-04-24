6840527da0da643a858343110a3184e82fe7548e
VuongTrinh <vuongtrinh.dev@gmail.com>
2021-04-24 15:39:54 +0700

    First commit.

---
+++ src/RazorPagesTestSample/Data/Message.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RazorPagesTestSample.Data
+{
+    public class Message
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [StringLength(200, ErrorMessage = "There's a 200 character limit on messages. Please shorten your message.")]
+        public string Text { get; set; }
+    }
+}
